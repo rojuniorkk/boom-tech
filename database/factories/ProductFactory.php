1f7ffb13c21f62f7ec9bcb86ece754abb72aa8fa
Rogério <ro.juniorkk@gmail.com>
2024-08-20 10:16:35 -0300

    padrão

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use App\Models\Company;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $name = $this->faker->name();
+
+        return [
+            'name' => $name,
+            'description' => $this->faker->paragraph(),
+            'price' => $this->faker->numberBetween(100, 1000),
+            'slug' => Str::slug($name),
+            'image' => $this->faker->imageUrl(640, 480),
+            'category_id' => Category::get()->random()->id,
+            'company_id' => Company::get()->random()->id
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,9 +15,15 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
+        $this->call([
+            CategorySeeder::class,
+            CompanySeeder::class,
+            ProductSeeder::class,
+        ]);
+
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'admin',
+            'email' => 'admin@email.com',
         ]);
     }
 }
